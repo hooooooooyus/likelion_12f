6fc1fb6753dd21aeb728b1c92e4925565e50a3ca
hyuns <hyunsu6542@naver.com>
2024-05-28 19:48:43 +0900

    7주차

---
+++ 7주차/app.js
@@ -0,0 +1,8 @@
+const bold = document.querySelector("b");
+bold.classList.add("border");
+bold.classList.add("purple");
+
+const image = document.createElement("img");
+image.src = "logo.png";
+image.style.width = "200px";
+document.body.appendChild(image);
